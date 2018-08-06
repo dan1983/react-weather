6e16b655f52873251544fe9ab4878036e0b7ca01
root <daniel.farnos.e@gmail.com>
2018-08-06 22:03:24 +0200

    call api

--- src/components/WheaterLocation/index.js
+++ src/components/WheaterLocation/index.js
@@ -2,6 +2,9 @@ import React , { Component } from 'react';
 import Location from './Location';
 import WeaterData from './WeatherData';
 import './Style.css';
+const location ="London,uk";
+const APP_KEY="0bed73889de8f5415f8ca768e929408f";
+const api_weather =`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APP_KEY}`
 
 
 const data ={
@@ -12,13 +15,7 @@ const data ={
 
 };
 
-const data1 ={
-        temperature:21,
-        weatherState:"sunny",
-        humidity:20,
-        wind:76
 
-};
 
 class WheaterLocation extends Component {
         constructor(){
@@ -30,13 +27,50 @@ class WheaterLocation extends Component {
                 }
 
         }
+
+        getWeaterState =(weather)=>{
+                return "sunny";
+
+        }
+
+         getData =(weaterData) => {
+               const { humidity,temp } = weaterData.main;
+               const { speed } = weaterData.wind;
+               const weaterState= this.getWeaterState(this.weather);
+
+               const data1 ={
+                temperature:temp,
+                weatherState: weaterState,
+                humidity:humidity,
+                wind: `${speed} m/s`
+        
+        };
+
+        return data1;
+
+
+        }
+        handleUpdateClick = () => {
+                fetch(api_weather).then(data => {
+                       
+                        return data.json();
+
+                }).then(weaterData => {
+                       const data = this.getData(weaterData);
+                       this.setState({data})
+                        
+
+                });
+
+         }
+         
  
         render () {
                 return (
                         <div className="WheaterLocation flex flex-direction-c">
                                 <Location city={this.state.city}/>
                                 <WeaterData data={this.state.data}/>
-                                <button>Click</button>
+                                <button onClick={this.handleUpdateClick}>Click</button>
                         </div>
                 )
         }
